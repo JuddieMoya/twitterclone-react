038ecf4a10001a2620307bee076f793c2a5a8faf
Kyle Hill <username@myEmail.com>
2020-09-03 12:54:54 -0400

    added users api to profile.js

--- src/react/components/Menu.js
+++ src/react/components/Menu.js
@@ -7,33 +7,7 @@ import { logout } from "../../redux";
 import userInfo from './user.json';
 
 class Menu extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      pictureLocation: '',
-      info: userInfo
-    }
-  }
-
-  componentDidMount() {
-    fetch("https://kwitter-api.herokuapp.com/users/" + this.props.user)
-      .then(response => response.json())
-      .then((data) => {
-        if(data.user.pictureLocation !== null) {
-          this.setState({pictureLocation: data.user.pictureLocation})
-          console.log(this.state.pictureLocation)
-        } else {
-          this.setState({pictureLocation: "https://st3.depositphotos.com/4111759/13425/v/450/depositphotos_134255588-stock-illustration-empty-photo-of-male-profile.jpg"})
-          console.log(this.state.pictureLocation)
-        }
-        let info = {...this.state.info}
-        info = data;
-        this.setState({ info })
-        console.log(this.state.info)
-      })
-      .catch(err => console.log(err))
-  }
+  
 
   handleLogout = event => {
     event.preventDefault();
@@ -45,8 +19,8 @@ class Menu extends React.Component {
     return (
       <div id="menu">
         <div id='info'>
-          <h2 className='userInfo' id='displayName'>{this.state.info.user.displayName}</h2>
-          <p className='userInfo'>{this.state.info.user.about}</p>
+          <h2 className='userInfo' id='displayName'>{this.props.displayName}</h2>
+          {/* <p className='userInfo'>{this.state.info.user.about}</p> */}
         </div>
         {this.props.isAuthenticated && (
           <div id="menu-links">
