143b7e9f9480707cfea042244b49d666fa60bb63
Kyle Hill <kyle.hill56@yahoo.com>
2020-09-08 14:17:36 +0000

    Revert "Merge branch 'users' into 'master'"
    This reverts merge request !4

--- src/redux/users.js
+++ src/redux/users.js
@@ -1,31 +1,23 @@
-// import { store } from "./";
+import { store } from "./";
 
 
+export const updateUser = (input) => {
+    let name = input.username;
+    store.dispatch(addUsername(name));
+}
 
+const addUsername = (name) => {
+    return {
+        type: 'CHANGE_USER',
+        text: name
+    }
+}
 
-
-
-
-// //dispatch
-// export const updateUser = (input) => {
-//     let name = input.username;
-//     store.dispatch(addUsername(name));
-// }
-
-// // action
-// const addUsername = (name) => {
-//     return {
-//         type: 'CHANGE_USER',
-//         text: name
-//     }
-// }
-
-// //reducer
-// export const changeUsernameReducer = (state = {user: ''}, action) => {
-//     switch(action.type) {
-//         case 'CHANGE_USER':
-//             return {user: action.text};
-//         default: 
-//             return state;
-//     }
-// }
+export const changeUsernameReducer = (state = {user: ''}, action) => {
+    switch(action.type) {
+        case 'CHANGE_USER':
+            return {user: action.text};
+        default: 
+            return state;
+    }
+}
