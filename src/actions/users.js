98226b73bc77fe073bdc6691526f694cea107707
Kyle Hill <username@myEmail.com>
2020-09-08 10:25:07 -0400

    add changes

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,7 +1,3 @@
-import { store } from "./"
-
-const url = domain + "/users";
-
 export const GET_USERS = "GET_USERS"
 export const GET_USERS_SUCCESS = "GET_USERS_SUCCESS"
 export const GET_USERS_FAIL = "GET_USERS_FAIL"
--- src/react/components/Menu.js
+++ src/react/components/Menu.js
@@ -6,27 +6,19 @@ import { updateUser } from "../../redux/users";
 import { logout } from "../../redux";
 import userInfo from './user.json';
 
-class Menu extends React.Component {
-  
 
-  // handleLogout = event => {
-  //   event.preventDefault();
-  //   updateUser('');
-  //   this.props.logout();
-  // };
-  
+class Menu extends React.Component {
+  handleLogout = event => {
+    event.preventDefault();
+    this.props.logout();
+  };
   render() {
     return (
       <div id="menu">
-        <div id='info'>
-          <h2 className='userInfo' id='displayName'>{this.props.displayName}</h2>
-          {/* <p className='userInfo'>{this.state.info.user.about}</p> */}
-        </div>
+        <h1>Kwitter</h1>
         {this.props.isAuthenticated && (
           <div id="menu-links">
-            <Link to="/">Profile</Link>
             <Link to="/messagefeed">Message Feed</Link>
-            <Link to="/settings">Settings</Link>
             <Link to="/" onClick={this.handleLogout}>
               Logout
             </Link>
@@ -36,6 +28,48 @@ class Menu extends React.Component {
     );
   }
 }
+export default connect(
+  state => ({
+    result: state.auth.logout.result,
+    loading: state.auth.logout.loading,
+    error: state.auth.logout.error
+  }),
+  { logout }
+)(Menu);
+
+
+
+
+// class Menu extends React.Component {
+  
+
+//   handleLogout = event => {
+//     event.preventDefault();
+//     updateUser('');
+//     this.props.logout();
+//   };
+  
+//   render() {
+//     return (
+//       <div id="menu">
+//         <div id='info'>
+//           <h2 className='userInfo' id='displayName'>{this.props.displayName}</h2>
+//           {/* <p className='userInfo'>{this.state.info.user.about}</p> */}
+//         </div>
+//         {this.props.isAuthenticated && (
+//           <div id="menu-links">
+//             <Link to="/">Profile</Link>
+//             <Link to="/messagefeed">Message Feed</Link>
+//             <Link to="/settings">Settings</Link>
+//             <Link to="/" onClick={this.handleLogout}>
+//               Logout
+//             </Link>
+//           </div>
+//         )}
+//       </div>
+//     );
+//   }
+// }
   
 //   render() {
 //     return (
@@ -54,11 +88,11 @@ class Menu extends React.Component {
 //   }
 // }
 
-export default connect(
-  state => ({
-    result: state.auth.logout.result,
-    loading: state.auth.logout.loading,
-    error: state.auth.logout.error
-  }),
-  { logout }
-)(Menu);
+// export default connect(
+//   state => ({
+//     result: state.auth.logout.result,
+//     loading: state.auth.logout.loading,
+//     error: state.auth.logout.error
+//   }),
+//   { logout }
+// )(Menu);
